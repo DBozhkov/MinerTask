0024c8bc53822d7e32feee850f4d7d4c699c250b
Dimitar Bozhkov <dimitarbzhkov@gmail.com>
2022-08-24 22:07:36 +0300

    Add project files.

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TempWork
+{
+    class Program
+    {
+        static void Main()
+        {
+            string command = string.Empty;
+            Dictionary<string, int> resourceCounter = new Dictionary<string, int>();
+            int quantity;
+            string tempKey = string.Empty;
+            int counter = 1;
+            while ((command = Console.ReadLine()) != "stop")
+            {
+                if (counter % 2 != 0)
+                {
+                    tempKey = command;
+                    if (!resourceCounter.ContainsKey(command))
+                    {  
+                        resourceCounter.Add(command, 0);
+                    }
+                }
+                if (counter % 2 == 0)
+                {
+                    
+                    quantity = int.Parse(command);
+                    if (resourceCounter.ContainsKey(tempKey))
+                    {
+                        resourceCounter[tempKey] += quantity;
+                    }
+                }
+                counter++;
+                
+            }
+
+            
+            foreach (KeyValuePair<string,int> pair in resourceCounter)
+            {
+                Console.WriteLine($"{pair.Key} -> {pair.Value}");
+            }
+        }
+    }
+}
---
+++ ConsoleApp7/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp7
+{
+    class Student
+    {
+        public Student(string name, int age, double grade)
+        {
+            Name = name;
+            Age = age;
+            Grade = grade;
+        }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public double Grade { get; set; }
+    }
+}
